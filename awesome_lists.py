79a6073349e76fe6cb8f05cb39fcc51731de9df9
Vladimir Milojevic <vladamx.dev@gmail.com>
2018-06-20 01:15:59 +0200

    Make pylint happy.

--- awesome_lists.py
+++ awesome_lists.py
@@ -1,20 +1,30 @@
+""" AwesomeLists service
+"""
 import base64
 import json
-import logging
 from urllib2 import urlopen, URLError
 from threading import Timer
 
 class AwesomeLists(object):
+    """AwesomeLists service provider"""
+    # We could maybe open this magic constant for user to configure
+    # but i think this is sensible default 
+    # value is in seconds
+    _cache_expire_period = 60*60*24
     def __init__(self, url, logger):
         self.url = url
         self.logger = logger
         self.awesome_lists = []
+        self.token = ''
 
     def load(self):
-        # fetch if cache  is empty
-        if len(self.awesome_lists) == 0 : 
+        """Load AwesomeLists from GitHub repo"""
+        # fetch only if cache  is empty
+        if not self.awesome_lists: 
             self.logger.info('Sending request for fresh awesome lists')
             try:
+                if not self.token:
+                    raise RuntimeError('Missing GitHub token')
                 response = urlopen(self.url + '?access_token=' + self.token)
                 json_response_body = json.loads(response.read())
                 base64_content = json_response_body["content"]
@@ -22,25 +32,25 @@ class AwesomeLists(object):
                 
                 self.awesome_lists = json.loads(raw_content)
     
-            except URLError as e:
-                self.logger.error(e)
+            except URLError as err:
+                self.logger.error(err)
             else:
                 if response != None:
                     response.close()
 
-                def onTime():
+                def on_time():
+                    """Timer calback"""
                     self.__invalidate_cache()
 
-                t = Timer(60*60*24*2, onTime)
-                t.start() 
+                time = Timer(type(self)._cache_expire_period, on_time)
+                time.start() 
     
     def set_token(self, token):
+        """Set GitHub token"""
         self.token = token
 
-    def __invalidate_cache(self):
-        self.awesome_lists = []
-
-    def get(self, search_term):
+    def search(self, search_term):
+        """Search for awesome lists"""
         term = search_term.strip().lower() if search_term != None else ''
         
         if term == '':
@@ -48,7 +58,10 @@ class AwesomeLists(object):
         
         return [aw for aw in self.awesome_lists if self.__match(aw, term)]
 
-    def __match(self, aw,  search_term):
-        subject = aw["name"] + aw.get("description", '')
+    def __match(self, list_item, search_term):
+        subject = list_item["name"] + list_item.get("description", '')
         
         return subject.lower().find(search_term) != -1
+    
+    def __invalidate_cache(self):
+        self.awesome_lists = []
--- main.py
+++ main.py
@@ -1,45 +1,49 @@
+"""AwesomeLists extension binding to Ulauncher
+"""
 import logging
-import hashlib
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
-from ulauncher.config import CACHE_DIR
-import json
 from awesome_lists import AwesomeLists
-from pprint import pprint, pformat
 
 LOGGER = logging.getLogger(__name__)
-env = {
-    "url": 'https://api.github.com/repos/lockys/Awesome.json/contents/all-github-path/sindresorhus-awesome.json',
+ENV = {
+    "url": 'https://api.github.com/repos/lockys/Awesome.json \
+    /contents/all-github-path/sindresorhus-awesome.json',
 }
-aw = AwesomeLists(env["url"], LOGGER)
+AWESOME_PROVIDER = AwesomeLists(ENV["url"], LOGGER)
+
 
 class AwesomeListsExtension(Extension):
+    """AwesomeLists extension entry point"""
 
     def __init__(self):
         LOGGER.info('init Awesome lists Extension')
-      
+
         super(AwesomeListsExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
- 
+
+
 class KeywordQueryEventListener(EventListener):
+    """Ulauncher Event Listener"""
 
     def on_event(self, event, extension):
+        """React to user input"""
         items = []
-    
+
         github_access_token = extension.preferences['github_access_token']
-        if len(github_access_token) == 0:
+        if not github_access_token:
             LOGGER.error("GitHub access token is required")
             return
-        
-        aw.set_token(github_access_token)
-        aw.load()
-        
-        lists = aw.get(event.get_argument())
-    
+
+        AWESOME_PROVIDER.set_token(github_access_token)
+        AWESOME_PROVIDER.load()
+
+        lists = AWESOME_PROVIDER.search(event.get_argument())
+
         for item in lists[:8]:
             items.append(ExtensionResultItem(
                 icon='images/icon.png',
@@ -48,8 +52,8 @@ class KeywordQueryEventListener(EventListener):
                 on_enter=OpenUrlAction(item["url"])
             ))
 
-
         return RenderResultListAction(items)
 
+
 if __name__ == '__main__':
-   AwesomeListsExtension().run()
+    AwesomeListsExtension().run()
